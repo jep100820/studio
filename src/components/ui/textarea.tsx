de6ac6f61e649e8efea004ef9850a1bba7dd9ca9
rodeo pahilga <rppahilga@gmail.com>
2025-08-04 09:06:21 +0000

    Highlight Gray?

--- src/components/ui/textarea.tsx
+++ src/components/ui/textarea.tsx
@@ -10,7 +10,7 @@ const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
     return (
       <textarea
         className={cn(
-          "flex w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:bg-secondary disabled:cursor-not-allowed disabled:opacity-50",
+          "flex w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:bg-primary/10 disabled:cursor-not-allowed disabled:opacity-50",
           "min-h-[40px] focus:min-h-[80px]",
           className
         )}
